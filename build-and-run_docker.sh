87689ac2746ba4e4ab2d0b4e6ab8660f5726af34
Maxim Osin <maxim.neitralov@gmail.com>
2024-01-23 19:12:29 +0700

    Скрипт сборки и запуска для Docker синхронизирован со скриптом для Podman

--- build-and-run_docker.sh
+++ build-and-run_docker.sh
@@ -3,7 +3,7 @@
 cd src/WebAPI
 
 echo "Компилируется проект..."
-dotnet build -c Release
+dotnet build -c Debug
 
 echo "Остановка предыдущего контейнера..."
 docker stop test
@@ -15,12 +15,10 @@ echo "Запускается контейнер с программой..."
 docker run \
 -d \
 --rm \
--p 8000:80 \
+-p 8000:8080 \
 -v database:/app/Database:Z \
 -e ASPNETCORE_ENVIRONMENT=Development \
 -e AppSettings:Token="My secret key. 128 bit at least." \
 -e ConnectionStrings:DefaultConnection="Data Source=Database/Database.db" \
 --name test \
 webapitest
-
-echo "Готово"
