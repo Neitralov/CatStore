73329941cbd11f99d3f1c62186b09afcff1c0cd2
Maxim Osin <maxim.neitralov@gmail.com>
2024-01-23 23:09:41 +0700

    Стандартный логгер заменен на Serilog

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,77 +1,105 @@
-var builder = WebApplication.CreateBuilder(args);
+var loggerConfiguration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(loggerConfiguration)
+    .CreateBootstrapLogger();
+
+try
 {
-    builder.Services.AddRouting(options => options.LowercaseUrls = true);
-    builder.Services.AddControllers();
-    builder.Services.AddHealthChecks();
-    
-    builder.Services.AddDbContextFactory<DatabaseContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    Log.Information("Starting web application");
 
-    builder.Services.AddTransient<ICatRepository, CatRepository>();
-    builder.Services.AddTransient<CatService>();
-    builder.Services.AddTransient<IUserRepository, UserRepository>();
-    builder.Services.AddTransient<UserService>();
-    builder.Services.AddTransient<ICartRepository, CartRepository>();
-    builder.Services.AddTransient<CartService>();
-    builder.Services.AddTransient<IOrderRepository, OrderRepository>();
-    builder.Services.AddTransient<OrderService>();
+    var builder = WebApplication.CreateBuilder(args);
+    {
+        builder.Host.UseSerilog((_, _, configuration) => configuration
+            .ReadFrom.Configuration(loggerConfiguration));
 
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+        builder.Services.AddHealthChecks();
 
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
+        builder.Services.AddDbContextFactory<DatabaseContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddTransient<ICatRepository, CatRepository>();
+        builder.Services.AddTransient<CatService>();
+        builder.Services.AddTransient<IUserRepository, UserRepository>();
+        builder.Services.AddTransient<UserService>();
+        builder.Services.AddTransient<ICartRepository, CartRepository>();
+        builder.Services.AddTransient<CartService>();
+        builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+        builder.Services.AddTransient<OrderService>();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
             {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
-                    .GetBytes(builder.Configuration["AppSettings:Token"] ?? throw new NullReferenceException())),
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                ValidateLifetime = true
-            };
-        });
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                        .GetBytes(builder.Configuration["AppSettings:Token"] ?? throw new NullReferenceException())),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true
+                };
+            });
 
-    builder.Services.AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo { Title = "CatStore.WebAPI", Version = "0.1.0" } );
-        
-        var xmlDocPaths = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList();
-        xmlDocPaths.ForEach(xmlDocPath => options.IncludeXmlComments(xmlDocPath));
-
-        var jwtSecurityScheme = new OpenApiSecurityScheme
+        builder.Services.AddSwaggerGen(options =>
         {
-            In = ParameterLocation.Header,
-            Description = "Please enter JWT token",
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = JwtBearerDefaults.AuthenticationScheme,
-        };
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "CatStore.WebAPI", Version = "0.1.0" } );
 
-        options.AddSecurityDefinition(jwtSecurityScheme.Scheme, jwtSecurityScheme);
-        options.OperationFilter<SecurityRequirementsOperationFilter>(true, JwtBearerDefaults.AuthenticationScheme);
-    });
+            var xmlDocPaths = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList();
+            xmlDocPaths.ForEach(xmlDocPath => options.IncludeXmlComments(xmlDocPath));
+
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter JWT token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            };
+
+            options.AddSecurityDefinition(jwtSecurityScheme.Scheme, jwtSecurityScheme);
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, JwtBearerDefaults.AuthenticationScheme);
+        });
+    }
+
+    var app = builder.Build();
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/error");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.MapHealthChecks("/health");
+        app.Run();
+    }
 }
-
-var app = builder.Build();
+catch (Exception exception)
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/error");
-    }
-
-    app.UseHttpsRedirection();
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.MapHealthChecks("/health");
-    app.Run();    
+    Log.Fatal(exception, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
