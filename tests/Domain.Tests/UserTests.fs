349f51b78d23d9cba939b4c5757fa51154b8da83
Maxim Osin <maxim.neitralov@gmail.com>
2024-04-01 19:47:43 +0700

    Произошел C# головного мозга

--- tests/Domain.Tests/UserTests.fs
+++ tests/Domain.Tests/UserTests.fs
@@ -8,30 +8,30 @@ open Domain.ServiceErrors
 [<InlineData("admin@gmail.com", "admin", "admin")>]
 [<InlineData("example@mail.ru", "1234", "1234")>]
 let ``Пользователь должен быть создан при корректных значениях`` email password confirmPassword =
-    let sut = User.Create(email, password, confirmPassword);
+    let sut = User.Create(email, password, confirmPassword)
     let result = sut.IsError
     Assert.False(result)
     
 [<Fact>]
 let ``Нельзя создать пользователя с некорректным email`` () =
-    let sut = User.Create("NotAnEmail", "1234", "1234");
+    let sut = User.Create("NotAnEmail", "1234", "1234")
     let result = sut.FirstError
     Assert.Equal(Errors.User.InvalidEmail, result)
     
 [<Fact>]
 let ``Нельзя создать пользователя с коротким паролем`` () =
-    let sut = User.Create("example@gmail.com", "123", "123");
+    let sut = User.Create("example@gmail.com", "123", "123")
     let result = sut.FirstError
     Assert.Equal(Errors.User.InvalidPassword, result)
     
 [<Fact>]
 let ``Нельзя создать пользователя если пароли в полях не совпадают`` () =
-    let sut = User.Create("example@gmail.com", "1234", "123Q");
+    let sut = User.Create("example@gmail.com", "1234", "123Q")
     let result = sut.FirstError
     Assert.Equal(Errors.User.PasswordsDontMatch, result)
     
 [<Fact>]
 let ``Корректный пароль пользователя нельзя сменить на короткий`` () =
-    let sut = User.Create("example@gmail.com", "1234", "1234");
-    let result = sut.Value.ChangePassword("123").FirstError;
+    let sut = User.Create("example@gmail.com", "1234", "1234")
+    let result = sut.Value.ChangePassword("123").FirstError
     Assert.Equal(Errors.User.InvalidPassword, result)
